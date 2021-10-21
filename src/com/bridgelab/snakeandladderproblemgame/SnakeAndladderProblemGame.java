45604ca5b1d461ce4e5eb01b7b2189b3b95cae40
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-21 16:52:31 +0530

    Added:Start Position is zero

---
+++ src/com/bridgelab/snakeandladderproblemgame/SnakeAndladderProblemGame.java
@@ -0,0 +1,17 @@
+package com.bridgelab.snakeandladderproblemgame;
+
+import java.util.Scanner;
+
+public class SnakeAndladderProblemGame {
+	static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Snake And ladder Problem Game");
+		toSnakeAndladderProblemGame();
+	}
+
+	public static void toSnakeAndladderProblemGame() {
+		int Position = 0;
+		System.out.println("Your Starting Position is : " + Position);
+	}
+}
