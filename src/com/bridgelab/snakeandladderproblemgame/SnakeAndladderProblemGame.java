9a2432e29f3edd0eac12fa572027ae820205fb0b
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-21 17:27:34 +0530

    Added:Use Random number to check options

--- src/com/bridgelab/snakeandladderproblemgame/SnakeAndladderProblemGame.java
+++ src/com/bridgelab/snakeandladderproblemgame/SnakeAndladderProblemGame.java
@@ -9,6 +9,7 @@ public class SnakeAndladderProblemGame {
 		System.out.println("Snake And ladder Problem Game");
 		toSnakeAndladderProblemGame();
 		toDice();
+		toRandomNumberToCheckOptions();
 	}
 
 	public static void toSnakeAndladderProblemGame() {
@@ -19,5 +20,35 @@ public class SnakeAndladderProblemGame {
 	public static void toDice() {
 		int toDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
 		System.out.println("Your Dice Value is : " + toDice);
+	}
+
+	public static void toRandomNumberToCheckOptions() {
+		int position = 0;
+		System.out.println("Your Starting Position is : " + position);
+		int die = (int) (Math.random() * 10) % 6 + 1;
+		System.out.println("dice value is " + die);
+		int value = (int) (Math.random() * 10) % 3 + 1;
+		System.out.println("case number is " + value);
+
+		switch (value) {
+		case 1:
+			System.out.println("No Play You are in the same position.");
+			break;
+
+		case 2:
+			position = (position + die);
+			System.out.println("your position after ladder is ::" + position);
+			break;
+
+		case 3:
+			position = (position - die);
+
+			if (position < 0) {
+				position = 0;
+			}
+			System.out.println("Your Position after snake is::" + position);
+			break;
+		}
+
 	}
 }
