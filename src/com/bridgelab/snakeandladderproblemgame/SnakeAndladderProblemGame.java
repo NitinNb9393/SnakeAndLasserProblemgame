72d7c632d28b9900336a5577f168db81d00ca9dd
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-21 17:15:16 +0530

    Added:Roll the dice to get between 1 to 6

--- src/com/bridgelab/snakeandladderproblemgame/SnakeAndladderProblemGame.java
+++ src/com/bridgelab/snakeandladderproblemgame/SnakeAndladderProblemGame.java
@@ -8,10 +8,16 @@ public class SnakeAndladderProblemGame {
 	public static void main(String[] args) {
 		System.out.println("Snake And ladder Problem Game");
 		toSnakeAndladderProblemGame();
+		toDice();
 	}
 
 	public static void toSnakeAndladderProblemGame() {
 		int Position = 0;
 		System.out.println("Your Starting Position is : " + Position);
+	}
+
+	public static void toDice() {
+		int toDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println("Your Dice Value is : " + toDice);
 	}
 }
