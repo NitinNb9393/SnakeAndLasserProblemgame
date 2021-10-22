02d281abb755fd43e3293cacd22f3cb434ac3505
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-22 14:59:44 +0530

    program reapeat till player reach to 100 position

--- src/com/bridgelab/snakeandladderproblemgame/SnakeAndladderProblemGame.java
+++ src/com/bridgelab/snakeandladderproblemgame/SnakeAndladderProblemGame.java
@@ -10,6 +10,7 @@ public class SnakeAndladderProblemGame {
 		toSnakeAndladderProblemGame();
 		toDice();
 		toRandomNumberToCheckOptions();
+		toPlayerReachToposition();
 	}
 
 	public static void toSnakeAndladderProblemGame() {
@@ -49,6 +50,43 @@ public class SnakeAndladderProblemGame {
 			System.out.println("Your Position after snake is::" + position);
 			break;
 		}
+	}
 
+	public static void toPlayerReachToposition() {
+		int startPosition = 0;
+		System.out.println("Player staring position is : " + startPosition);
+
+		int NO_PLAY = 0;
+		int is_Ladder = 1;
+		int is_Snake = 2;
+		int position = 0;
+
+		while (position < 100) {
+
+			int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+			System.out.println("diceNumber is : " + diceNumber);
+
+			int option = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("selected player option : " + option);
+
+			switch (option) {
+			case 1:
+				position = diceNumber + position;
+				System.out.println("position for the player after the ladder is : " + position);
+				break;
+
+			case 2:
+				position = position - diceNumber;
+				if (position < 0) {
+					position = 0;
+				}
+
+				System.out.println("position for the player after the Snake is : " + position);
+				break;
+
+			default:
+				System.out.println("No play");
+			}
+		}
 	}
 }
